c311dcd52a6ac55ee36bf64fc0911f3f14369886
bernardthibaut <thibaut.t.bernard@gmail.com>
2016-04-07 10:21:52 +0200

    First try for the zoom

--- src/gl/Viewer.java
+++ src/gl/Viewer.java
@@ -1,45 +1,76 @@
 package gl;
 
 import java.awt.BorderLayout;
+import java.awt.event.ContainerEvent;
+import java.awt.event.ContainerListener;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseWheelEvent;
+import java.awt.event.MouseWheelListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+import java.awt.event.WindowStateListener;
 
 import javax.swing.JFrame;
 
 import org.opencv.core.Mat;
 
+import com.jogamp.newt.event.KeyAdapter;
+import com.jogamp.newt.event.KeyEvent;
 import com.jogamp.opengl.GL;
 import com.jogamp.opengl.GL2;
 import com.jogamp.opengl.GLAutoDrawable;
 import com.jogamp.opengl.GLEventListener;
 import com.jogamp.opengl.awt.GLCanvas;
+import com.jogamp.opengl.glu.GLU;
 import com.jogamp.opengl.util.FPSAnimator;
 
-public class Viewer implements GLEventListener{
+public class Viewer implements GLEventListener {
+
+	private GLU glu;
+	float z = 0;
 
 	private Mat pointCloud;
-	
-	public Viewer(GLCanvas canvas, Mat pointCloud) {	
+
+	public Viewer(GLCanvas canvas, Mat pointCloud) {
 		this.pointCloud = pointCloud;
 		JFrame frame = new JFrame("JOGL Program");
 		frame.setLayout(new BorderLayout());
 		frame.setSize(800, 800);
-		frame.add(canvas);
 		
+		frame.add(canvas);
+
+		frame.addMouseWheelListener(new MouseWheelListener() {
+
+			@Override
+			public void mouseWheelMoved(MouseWheelEvent e) {
+				if (e.getWheelRotation() == 1)
+					z = 1;
+				else
+					z = -1;
+			}
+		});
+
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+
 		FPSAnimator animator = new FPSAnimator(canvas, 60);
 		animator.start();
 	}
-	
+
 	private void update() {
 		// Put the changes to do (ex: change the coordonate of a point)
 	}
 
 	private void render(GLAutoDrawable drawable) {
 		GL2 gl = drawable.getGL().getGL2();
-		gl.glClear(GL.GL_COLOR_BUFFER_BIT);
-		gl.glRotatef(2f, 0, 1, 0);
-		
+		gl.glPushMatrix();
+		gl.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT);
+
+		gl.glTranslatef(0, 0, z);
+		z = 0;
+
+		// gl.glRotatef(2f, 0, 1, 0);
+
 		gl.glBegin(GL2.GL_POINTS);
 		for (int i = 0; i < pointCloud.rows(); i++) {
 			gl.glColor3d(0, 1, 0);
@@ -47,7 +78,7 @@ public class Viewer implements GLEventListener{
 		}
 		gl.glEnd();
 	}
-	
+
 	@Override
 	public void display(GLAutoDrawable drawable) {
 		update();
@@ -57,19 +88,26 @@ public class Viewer implements GLEventListener{
 	@Override
 	public void dispose(GLAutoDrawable drawable) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void init(GLAutoDrawable drawable) {
-		// TODO Auto-generated method stub
-		
+		glu = new GLU();
 	}
 
 	@Override
 	public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
-		// TODO Auto-generated method stub
-		
+		GL2 gl = drawable.getGL().getGL2();
+
+		gl.glMatrixMode(GL2.GL_PROJECTION);
+		gl.glLoadIdentity();
+		float aspect = width / height;
+		glu.gluPerspective(3.0, aspect, 1.0, 300.0);
+
+		gl.glMatrixMode(GL2.GL_MODELVIEW);
+		gl.glLoadIdentity();
+		glu.gluLookAt(0.0, 0.0, 10.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
 	}
 
 }
--- src/launch/Launcher.java
+++ src/launch/Launcher.java
@@ -7,20 +7,20 @@ import com.jogamp.opengl.awt.GLCanvas;
 import gl.Viewer;
 import pcdLoader.Finder;
 import pcdLoader.PCDFile;
+import pcdLoader.Reader;
 
 public class Launcher {
 
 	public static void main(String[] args) {
 		Finder.setLookAndFeel();
-		PCDFile file = Finder.findFile();
+		// PCDFile file = Finder.findFile();
+		PCDFile file = Reader.readFile("res/bunny.pcd");
 		
 		GLProfile.initSingleton();
 		GLProfile glp = GLProfile.getDefault();
 		GLCapabilities caps = new GLCapabilities(glp);
 		GLCanvas canvas = new GLCanvas(caps);
 		canvas.addGLEventListener(new Viewer(canvas, file.getData()));
-		
-		System.out.println(file.getData().dump());
 	}
 	
 }
